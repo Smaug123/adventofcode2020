6de1d1d8aab36c26a190fcc6fc42032190673629
Smaug123 <patrick+github@patrickstevens.co.uk>
2020-12-09 09:21:57 +0000

    Day 9

---
+++ AdventOfCode/Day9.fs
@@ -0,0 +1,113 @@
+namespace AdventOfCode
+
+open AdventOfCode.Internals
+
+[<RequireQualifiedAccess>]
+module Day9 =
+
+    type AvailableSums<'a> =
+        {
+            /// This is best explained by example. See the unit test. This is basically a collection of ring buffers,
+            /// which essentially moves a triangular window of valid answers across the torus represented by the 2D
+            /// array.
+            Buffer : 'a [] []
+            mutable Ptr : int
+            Modulus : int
+            Numbers : 'a []
+        }
+
+    [<RequireQualifiedAccess>]
+    module AvailableSums =
+
+        /// Couldn't get statically resolved type parameters and a (+) constraint to work straight away, so just
+        /// did it old-school
+        let create<'a> (plus : 'a -> 'a -> 'a) (preamble : 'a seq) (n : int) : AvailableSums<'a> =
+            let preamble = Seq.toArray preamble
+
+            let buffer = [| for _ in 0..n-1 do yield Array.zeroCreate n |]
+            for i in 0..n-1 do
+                let whichBuffer = buffer.[i]
+                for j in 0..(n - i - 1) do
+                    whichBuffer.[j] <- plus (preamble.[i]) (preamble.[(j + i) % n])
+
+            {
+                Modulus = n
+                Ptr = 0
+                Buffer = buffer
+                Numbers = preamble
+            }
+
+        /// List the available sums. Order is well-defined by the two elements a number is reached from:
+        /// * Sums reachable from a more recent element come earlier;
+        /// * Within the sums reachable from the same element, sums reachable from a more recent element come earlier.
+        let toSeq<'a> (s : AvailableSums<'a>) : 'a seq =
+            seq {
+                for i in 0..s.Modulus - 1 do
+                    let whichBuffer = s.Buffer.[(i + s.Ptr) % s.Modulus]
+                    for j in 0..(s.Modulus - i - 1) do
+                        yield whichBuffer.[j]
+            }
+
+        /// Incorporate the next number in the stream, discarding the initial one.
+        let advance (plus : 'a -> 'a -> 'a) (i : 'a) (s : AvailableSums<'a>) : unit =
+            s.Numbers.[s.Ptr] <- i
+            for count in 1..s.Modulus do
+                s.Buffer.[(s.Ptr + count - 1) % s.Modulus].[(s.Modulus - count + 1) % s.Modulus] <- plus i s.Numbers.[(s.Ptr + count - 1) % s.Modulus]
+            s.Ptr <- (s.Ptr + 1) % s.Modulus
+
+        let isAvailable (i : 'a) (s : AvailableSums<'a>) : bool =
+            toSeq s
+            |> Seq.contains i
+
+    let part1 () =
+        let numbers =
+            Utils.readResource "Day9Input.txt"
+            |> List.map int64
+        let available = AvailableSums.create<int64> (+) numbers.[0..24] 25
+        let rec go (rest : int64 list) =
+            match rest with
+            | [] -> failwith "Hit the end"
+            | r :: rest ->
+                if AvailableSums.isAvailable r available then
+                    AvailableSums.advance (+) r available
+                    go rest
+                else
+                    r
+        go numbers.[25..]
+
+    let tryMinAndMax<'a when 'a : comparison> (s : 'a seq) : ('a * 'a) option =
+        use s = s.GetEnumerator ()
+        if not <| s.MoveNext () then None else
+        let mutable min = s.Current
+        let mutable max = s.Current
+        while s.MoveNext () do
+            if s.Current < min then min <- s.Current else
+            if s.Current > max then max <- s.Current
+
+        Some (min, max)
+
+    let part2 () =
+        let badNumber = part1 ()
+        let numbers =
+            Utils.readResource "Day9Input.txt"
+            |> List.map int64
+            |> List.toArray
+
+        assert(numbers |> Array.forall (fun i -> i >= 0L))
+
+        let rec go (target : int64) (ptr : int) =
+            if ptr >= numbers.Length then failwith "hit the end"
+            let rec go2 (sum : int64) (endPoint : int) =
+                if endPoint >= numbers.Length then None else
+                let sum = sum + numbers.[endPoint]
+                if sum = target then Some endPoint else
+                go2 sum (endPoint + 1)
+
+            match go2 0L ptr with
+            | None -> go target (ptr + 1)
+            | Some endPoint -> (ptr, endPoint)
+
+        let start, endPoint = go badNumber 0
+        match tryMinAndMax numbers.[start..endPoint] with
+        | None -> failwith "empty seq"
+        | Some (min, max) -> max + min
---
+++ Test/TestDay9.fs
@@ -0,0 +1,97 @@
+namespace Test
+
+open AdventOfCode
+open FsUnitTyped
+open NUnit.Framework
+
+[<TestFixture>]
+module TestDay9 =
+
+    [<Test>]
+    let ``Test availableSums advancement`` () =
+        let available = Day9.AvailableSums.create (+) [ 30 ; 20 ; 15 ; 25 ; 47 ] 5
+        available.Modulus |> shouldEqual 5
+        available.Ptr |> shouldEqual 0
+        available.Buffer
+        |> shouldEqual
+            [|
+                // 30 plus everything
+                [| 60 ; 50 ; 45 ; 55 ; 77 |]
+                // 20 plus everything starting from 20 (the last element is junk)
+                [| 40 ; 35 ; 45 ; 67 ; 0 |]
+                // 15 plus everything starting from 15 (the last two elements are junk)
+                [| 30 ; 40 ; 62 ; 0 ; 0 |]
+                // 25 plus everything (the last three elements are junk)
+                [| 50 ; 72 ; 0 ; 0 ; 0 |]
+                [| 94 ; 0 ; 0 ; 0 ; 0 |]
+            |]
+
+        Day9.AvailableSums.advance (+) 40 available
+
+        available.Modulus |> shouldEqual 5
+        available.Ptr |> shouldEqual 1
+        available.Buffer
+        |> shouldEqual
+            [|
+                // 40 plus everything; the last four elements are junk.
+                [| 80 ; 50 ; 45 ; 55 ; 77 |]
+                // 20 plus everything starting from 20 (the last element has the new 40 added to it)
+                [| 40 ; 35 ; 45 ; 67 ; 60 |]
+                // 15 plus everything starting from 15 (the last element is junk; the second-to-last has the new 40)
+                [| 30 ; 40 ; 62 ; 55 ; 0 |]
+                // 25 plus everything (the last two elements are junk; the third-to-last has the new 40)
+                [| 50 ; 72 ; 65 ; 0 ; 0 |]
+                // 47 plus everything (the last three elements are junk)
+                [| 94 ; 87 ; 0 ; 0 ; 0 |]
+            |]
+
+        Day9.AvailableSums.advance (+) 62 available
+
+        available.Modulus |> shouldEqual 5
+        available.Ptr |> shouldEqual 2
+        available.Buffer
+        |> shouldEqual
+            [|
+                // 40 plus everything; the last three elements are junk.
+                [| 80 ; 102 ; 45 ; 55 ; 77 |]
+                // 62 plus everything starting from 62 (the last four elements are junk)
+                [| 124 ; 35 ; 45 ; 67 ; 60 |]
+                // 15 plus everything starting from 15 (all elements are valid)
+                [| 30 ; 40 ; 62 ; 55 ; 77 |]
+                // 25 plus everything (the last element is junk)
+                [| 50 ; 72 ; 65 ; 87 ; 0 |]
+                // 47 plus everything (the last two elements are junk)
+                [| 94 ; 87 ; 109 ; 0 ; 0 |]
+            |]
+
+    [<Test>]
+    let ``Test availableSums toSeq`` () =
+        let available = Day9.AvailableSums.create (+) [ 30 ; 20 ; 15 ; 25 ; 47 ] 5
+
+        Day9.AvailableSums.advance (+) 40 available
+        Day9.AvailableSums.advance (+) 62 available
+
+        Day9.AvailableSums.toSeq available
+        |> List.ofSeq
+        |> shouldEqual
+            [
+                // 15 plus everything-ahead-of-15
+                30 ; 40 ; 62 ; 55 ; 77
+                // 15 plus everything-ahead-of-15
+                50 ; 72 ; 65 ; 87
+                // 25 plus everything-ahead
+                94 ; 87 ; 109
+                // 40 plus everything-ahead
+                80 ; 102
+                124
+            ]
+
+    [<Test>]
+    let ``Test part 1`` () =
+        Day9.part1 () |> shouldEqual 373803594L
+
+    [<Test>]
+    let ``Test part 2`` () =
+        Day9.part2 () |> shouldEqual 51152360L
+
+
